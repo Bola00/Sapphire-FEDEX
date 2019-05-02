c510a8a6014b887cd2723af2b684693dd6780ae2
tothlilla <toth.lillla88@gmail.com>
2019-05-02 14:31:31 +0200

    feat(detectCircles): finish displayResultimage() and useWebcam()

--- FedEx/FedEx/circle_detection.cpp
+++ FedEx/FedEx/circle_detection.cpp
@@ -2,13 +2,23 @@
 
 int detectAndCountCircles(cv::Mat image)
 {
+	int detectedCircles = 0;
+	displayResultimage(image);
 
-}
-void displayResultimage(cv::Mat image)
-{
-
+	return detectedCircles;
 }
 cv::Mat useWebcam()
 {
+	cv::Mat webCamImage;
+	cv::VideoCapture webCam;
+	webCam.open(0);
+	webCam >> webCamImage;
 
+	return webCamImage;
+}
+void displayResultimage(cv::Mat image)
+{
+	cv::namedWindow("Circles", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Circles", image);
+	cv::waitKey(0);
 }
--- FedEx/FedEx/fedex_project.cpp
+++ FedEx/FedEx/fedex_project.cpp
@@ -1,6 +1,8 @@
 #include "fedex_project.h"
+#include "circle_detection.h"
 
 void questionnaireFramework()
 {
-
+	cv::Mat webCamImage = useWebcam();
+	int detectedCircles = detectAndCountCircles(webCamImage);
 }
