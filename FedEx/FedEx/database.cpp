8e05bdedb206ee169a5a8f218741efd9f0361e7b
Bola00 <bogarlaszlo95@gmail.com>
2019-05-02 17:41:13 +0200

    feat(createDataBase)database with empty tables

--- FedEx/FedEx/database.cpp
+++ FedEx/FedEx/database.cpp
@@ -44,25 +44,170 @@ else {
 }
 
 /* Create SQL statement */
-sql = "CREATE TABLE QUESTION_1("  \
+sql = "CREATE TABLE WAKE_UP_HOUR("  \
 "ID INT PRIMARY KEY     NOT NULL," \
-"NUMBER_ONE           INT    NOT NULL," \
-"NUMBER_TWO           INT     NOT NULL," \
-"NUMBER_THREE          INT     NOT NULL," \
-"NUMBER_FOUR          INT     NOT NULL," \
-"NUMBER_FIVE           INT     NOT NULL);";
+"DATE          TEXT     NOT NULL);";
 
 /* Execute SQL statement */
 makeTable(rc, db, sql, zErrMsg);
-/*rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
 
-if (rc != SQLITE_OK) {
-	fprintf(stderr, "SQL error: %s\n", zErrMsg);
-	sqlite3_free(zErrMsg);
-}
-else {
-	fprintf(stdout, "Table created successfully\n");
-}*/
+/* Create SQL statement */
+sql = "CREATE TABLE BASE_QUESTION_1("  \
+"ID INT PRIMARY KEY           NOT NULL," \
+"NUMBER_ONE           INT     NOT NULL," \
+"NUMBER_TWO           INT     NOT NULL," \
+"NUMBER_THREE         INT     NOT NULL," \
+"NUMBER_FOUR          INT     NOT NULL," \
+"NUMBER_FIVE          INT     NOT NULL);";
+
+/* Execute SQL statement */
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE BASE_QUESTION_2("  \
+"ID INT PRIMARY KEY           NOT NULL," \
+"NUMBER_ONE           INT	  NOT NULL," \
+"NUMBER_TWO           INT     NOT NULL," \
+"NUMBER_THREE         INT     NOT NULL," \
+"NUMBER_FOUR          INT     NOT NULL," \
+"NUMBER_FIVE          INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE BASE_QUESTION_3("  \
+"ID INT PRIMARY KEY           NOT NULL," \
+"NUMBER_ONE           INT     NOT NULL," \
+"NUMBER_TWO           INT     NOT NULL," \
+"NUMBER_THREE         INT     NOT NULL," \
+"NUMBER_FOUR          INT     NOT NULL," \
+"NUMBER_FIVE          INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE WHERE_DO_YOU_KNOW_CR("  \
+"ID INT PRIMARY KEY      NOT NULL," \
+"SHOOL           INT     NOT NULL," \
+"PARENTS         INT     NOT NULL," \
+"FAMILY          INT     NOT NULL," \
+"FRIENDS         INT     NOT NULL," \
+"UNICEF          INT     NOT NULL," \
+"INTERNET        INT     NOT NULL," \
+"TV				 INT     NOT NULL," \
+"OTHER           INT     NOT NULL," \
+"NOWHERE         INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE MOST_USEFULL_OF_WAKEUP("  \
+"ID INT PRIMARY KEY             NOT NULL," \
+"CR						INT     NOT NULL," \
+"POOR_CHILDREN          INT     NOT NULL," \
+"WORK_OF_UNICEF         INT     NOT NULL," \
+"INTERNET_BULLYING      INT     NOT NULL," \
+"HELPING				INT     NOT NULL," \
+"GAME					INT     NOT NULL," \
+"MOVIE					INT     NOT NULL," \
+"EVERYTHING				INT     NOT NULL," \
+"NONE_OF_THEM           INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE LIKE_THE_MOST_OF_WAKEUP("  \
+"ID INT PRIMARY KEY             NOT NULL," \
+"CR						INT     NOT NULL," \
+"POOR_CHILDREN          INT     NOT NULL," \
+"WORK_OF_UNICEF         INT     NOT NULL," \
+"INTERNET_BULLYING      INT     NOT NULL," \
+"HELPING				INT     NOT NULL," \
+"GAME					INT     NOT NULL," \
+"MOVIE					INT     NOT NULL," \
+"EVERYTHING				INT     NOT NULL," \
+"NONE_OF_THEM           INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE NEED_MORE_TIME("  \
+"ID INT PRIMARY KEY             NOT NULL," \
+"CR						INT     NOT NULL," \
+"POOR_CHILDREN          INT     NOT NULL," \
+"WORK_OF_UNICEF         INT     NOT NULL," \
+"INTERNET_BULLYING      INT     NOT NULL," \
+"HELPING				INT     NOT NULL," \
+"GAME					INT     NOT NULL," \
+"MOVIE					INT     NOT NULL," \
+"EVERYTHING				INT     NOT NULL," \
+"NONE_OF_THEM           INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE NEED_LESS_TIME("  \
+"ID INT PRIMARY KEY             NOT NULL," \
+"CR						INT     NOT NULL," \
+"POOR_CHILDREN          INT     NOT NULL," \
+"WORK_OF_UNICEF         INT     NOT NULL," \
+"INTERNET_BULLYING      INT     NOT NULL," \
+"HELPING				INT     NOT NULL," \
+"GAME					INT     NOT NULL," \
+"MOVIE					INT     NOT NULL," \
+"EVERYTHING				INT     NOT NULL," \
+"NONE_OF_THEM           INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE QUIZ_ONE("  \
+"ID INT PRIMARY KEY     NOT NULL," \
+"NUMBER_ONE		INT     NOT NULL," \
+"NUMBER_TWO     INT     NOT NULL," \
+"NUMBER_THREE   INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE QUIZ_TWO("  \
+"ID INT PRIMARY KEY     NOT NULL," \
+"NUMBER_ONE		INT     NOT NULL," \
+"NUMBER_TWO     INT     NOT NULL," \
+"NUMBER_THREE   INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE QUIZ_THREE("  \
+"ID INT PRIMARY KEY     NOT NULL," \
+"NUMBER_ONE		INT     NOT NULL," \
+"NUMBER_TWO     INT     NOT NULL," \
+"NUMBER_THREE   INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE QUIZ_FOUR("  \
+"ID INT PRIMARY KEY     NOT NULL," \
+"NUMBER_ONE		INT     NOT NULL," \
+"NUMBER_TWO     INT     NOT NULL," \
+"NUMBER_THREE   INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE QUIZ_FIVE("  \
+"ID INT PRIMARY KEY     NOT NULL," \
+"NUMBER_ONE		INT     NOT NULL," \
+"NUMBER_TWO     INT     NOT NULL," \
+"NUMBER_THREE   INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE QUIZ_SIX("  \
+"ID INT PRIMARY KEY     NOT NULL," \
+"NUMBER_ONE		INT     NOT NULL," \
+"NUMBER_TWO     INT     NOT NULL," \
+"NUMBER_THREE   INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
+
+sql = "CREATE TABLE QUIZ_SEVEN("  \
+"ID INT PRIMARY KEY     NOT NULL," \
+"NUMBER_ONE		INT     NOT NULL," \
+"NUMBER_TWO     INT     NOT NULL," \
+"NUMBER_THREE   INT     NOT NULL);";
+
+makeTable(rc, db, sql, zErrMsg);
 
 sqlite3_close(db);
 return(0);
